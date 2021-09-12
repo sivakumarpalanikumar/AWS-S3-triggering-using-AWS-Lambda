4d2e12fc99209a97176c4c7b1fe824e0f7a0dc10
Sivakumar P <72782266+sivakumarpalanikumar@users.noreply.github.com>
2021-09-12 18:37:02 +0530

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,12 @@
+import boto3
+
+def lambda_handler (event, context):
+    s3_user=boto3.client('s3')
+    file=s3_user.list_objects (Bucket='dany-cse')['Contents'][0]['Key']
+    print(file) 
+    sns_user=boto3.client('sns')
+    response=sns_user.publish(
+    TopicArn='arn:aws:sns:us-east-1:128959570200:email-notify', 
+    Message="Processing of "+ file+" is completed",
+    Subject='53 Alert !!!',)
+    return
